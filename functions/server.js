af711d44c8c6f2216cec6b56a606efd788964e2f
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-15 12:53:42 -0500

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,13 +1,14 @@
 const express = require( 'express' );
 const expressServer = express();
 const objTimeString = require( '../time.json' );
+const bot = ( ENV.BOT_USERNAME || 'Server' );
+const botPort = ( ENV.PORT || 3000 );
 var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
 
 expressServer.all( '/', ( req, res ) => { res.send( bot + ' was last restarted: ' + strNow() ); } );
 
 function keepAlive() {
-  const bot = ( process.env.BOT_USERNAME || 'Server' );
-  var server = expressServer.listen( 3000, () => {
+  var server = expressServer.listen( botPort, () => {
     const port = server.address().port;
     console.log( '%s is ready on port %s.', bot, port );
   } );
